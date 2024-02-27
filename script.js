30a2a95714299a31f6fb2a4f918bb5bf210f0f13
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-27 14:56:23 +0700

    feat: video #1.2 done

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 */
 
 // 1. Object Literal
+// ! PROBLEM : Tidak efektif untuk object yang banyak
 let mhs = {
   name: "Your Name",
   energy: 10,
@@ -20,11 +21,11 @@ function Mhs(name, energy) {
   mhs.name = name;
   mhs.energy = energy;
 
+  // ! PROBLEM : Fungsi akan dibuat berulang kali setiap ada instansiasi object baru (memory space akan terbuang)
   mhs.makan = (porsi) => {
     this.energy += porsi;
     console.log(`Halo ${this.name}, selamat makan!`);
   };
-
   mhs.main = (jam) => {
     this.energy -= jam;
     console.log(`Halo ${this.name}, selamat bermain!`);
@@ -54,3 +55,27 @@ function Mahasiswa(name, energy) {
 
 let mahasiswa1 = new Mahasiswa("Your Name", 10);
 let mahasiswa2 = new Mahasiswa("Your Name", 20);
+
+// 4. Object.create()
+const methodMahasiswa = {
+  makan: (porsi) => {
+    this.energy += porsi;
+    console.log(`Halo ${this.name}, selamat makan!`);
+  },
+  main: (jam) => {
+    this.energy -= jam;
+    console.log(`Halo ${this.name}, selamat bermain!`);
+  },
+  tidur: (jam) => {
+    this.energy += jam * 2;
+    console.log(`Halo ${this.name}, selamat tidur!`);
+  },
+};
+
+function mhs(name, energy) {
+  let mhs = Object.create(methodMahasiswa);
+  mhs.name = name;
+  mhs.energy = energy;
+
+  return mhs;
+}
