652ca810f8b5df8fc09f565cac843d887daf6d11
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-27 22:55:32 +0700

    feat: video #2.2  done

--- script.js
+++ script.js
@@ -137,3 +137,46 @@ class StudentClass {
 }
 
 let student2 = new StudentClass("Your Name", 10);
+
+/*
+==================
+==== CLOSURE =====
+==================
+*/
+
+function init() {
+  let name = "Your Name"; // local variable
+  // inner function (closure*)
+  function tampilName() {
+    console.log(name); // access to parent variable
+  }
+  tampilName();
+}
+
+init();
+
+// ===== FUNCTION FACTORIES ========
+function ucapkanSalam(waktu) {
+  return (nama) => {
+    console.log(`Halo ${nama}, selamat ${waktu}`);
+  };
+}
+
+let selamatPagi = ucapkanSalam("Pagi");
+let selamatSiang = ucapkanSalam("Siang");
+let selamatMalam = ucapkanSalam("Malam");
+
+selamatPagi("Namamu");
+selamatSiang("Nama dia");
+
+// ===== PRIVATE METHOD ========
+let add = (() => {
+  let counter = 0;
+  return () => {
+    return ++counter;
+  };
+})(); // immediately invoked function expression (IIFE) => (function(){....})()
+
+console.log(add());
+console.log(add());
+console.log(add());
