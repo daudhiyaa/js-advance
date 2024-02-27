f3c97f3a10655562cc8995695cde1b3a489c2b25
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-27 17:25:01 +0700

    feat: video #1.3 done

--- script.js
+++ script.js
@@ -79,3 +79,61 @@ function mhs(name, energy) {
 
   return mhs;
 }
+
+/*
+====================
+==== PROTOTYPE =====
+====================
+*/
+
+function Student(name, energy) {
+  this.name = name;
+  this.energy = energy;
+}
+
+Student.prototype.makan = (porsi) => {
+  this.energy += porsi;
+  return `Halo ${this.name}, selamat makan!`;
+};
+
+Student.prototype.main = (jam) => {
+  this.energy -= jam;
+  return `Halo ${this.name}, selamat bermain!`;
+};
+
+Student.prototype.tidur = (jam) => {
+  this.energy += jam * 2;
+  return `Halo ${this.name}, selamat tidur!`;
+};
+
+let student1 = new Student("Your Name", 10);
+
+/*
+================
+==== CLASS =====
+================
+*/
+
+class StudentClass {
+  constructor(name, energy) {
+    this.name = name;
+    this.energy = energy;
+  }
+
+  makan(porsi) {
+    this.energy += porsi;
+    return `Halo ${this.name}, selamat makan!`;
+  }
+
+  main(jam) {
+    this.energy -= jam;
+    return `Halo ${this.name}, selamat bermain!`;
+  }
+
+  tidur(jam) {
+    this.energy += jam * 2;
+    return `Halo ${this.name}, selamat tidur!`;
+  }
+}
+
+let student2 = new StudentClass("Your Name", 10);
