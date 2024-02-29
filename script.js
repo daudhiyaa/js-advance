83b77201c9e1274da6f19b5ea4294d2a7696b5f5
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-29 15:24:53 +0700

    feat: video #5.1 done

--- script.js
+++ script.js
@@ -287,3 +287,37 @@ const result = angka
   .filter((a) => a > 5)
   .map((a) => a * 3)
   .reduce((a, c) => a + c, startValue);
+
+/*
+===========================
+==== TEMPLATE LITERALS ====
+===========================
+*/
+
+// 1. Multi-line String
+const multiLine = `Halo
+  nama saya 
+  ....`;
+
+// 2. Embedded Expression
+const namamu = "Your Name";
+const age = 20;
+const hello = `Halo, nama saya ${namamu}, 
+  saya berusia ${age} tahun`;
+
+// 3. HTML Fragments
+const mhs = {
+  name: "Your Name",
+  age: 20,
+  email: "yourname@gmail.com",
+};
+
+const el = `<div class="mhs">
+  <h2>${mhs.name}</h2>
+  <span class="age">${mhs.age}</span>
+  <p>Email : ${mhs.email}</p>`;
+
+// 4. Expression Interpolation
+const a = 10;
+const b = 15;
+const res = `Hasil penjumlahan ${a} + ${b} = ${a + b}`;
